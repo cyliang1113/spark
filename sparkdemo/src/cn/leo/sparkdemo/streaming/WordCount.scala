1b2719c73032bad48b4e2b36bd5022a2f54e30af
cyliang1113 <cyliang1113@126.com>
2016-09-16 10:49:18 +0800

    streaming

---
+++ sparkdemo/src/cn/leo/sparkdemo/streaming/WordCount.scala
@@ -0,0 +1,21 @@
+package cn.leo.sparkdemo.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.Seconds
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("WordCount")
+
+    val streamingContext = new StreamingContext(conf, Seconds(1))
+    
+    val lines = streamingContext.socketTextStream("hadoop23", 9999)
+    
+    lines.flatMap(x => x.split(" ")).map((_, 1)).reduceByKey((_ + _)).print()
+    
+    streamingContext.start
+    streamingContext.awaitTermination
+  }
+}
