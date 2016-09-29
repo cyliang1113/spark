b080e5373f84f8f5dffb5df5822641f9d6f05414
cyliang1113 <cyliang1113@126.com>
2016-09-29 11:12:39 +0800

    function

--- scalademo/src/cn/scalademo/functionprogram/Demo.scala
+++ scalademo/src/cn/scalademo/functionprogram/Demo.scala
@@ -2,15 +2,24 @@ package cn.scalademo.functionprogram
 
 object Demo {
   def main(args: Array[String]): Unit = {
-    sum1(0)(1, 2)
-    sum2(1)
+//    sum1(0)(1, 2)
+//    sum2(1)
+     var fun = sum3(10)
+     println(fun())
   }
 
-  def sum1(i: Int) = {
-    println(i)
-    (a: Int, b: Int) => println("a: " + a + ", b: " + b)
-  }
+//  def sum1(i: Int) = {
+//    println(i)
+//    (a: Int, b: Int) => println("a: " + a + ", b: " + b)
+//  }
+//
+//  var sum2 = sum1(_)
 
-  var sum2 = sum1(_)
+  def sum3(i: Int) = {
+    () =>
+      {
+        i + 1
+      }
+  }
 
 }
